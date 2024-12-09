d61ee1ca4527d09913348231c8b8657a67115a96
tclement0922 <git@tclement.dev>
2024-12-09 15:37:59 +0100

    Suppression Id manga

--- src/main/kotlin/montaine/guillaume/Manga.kt
+++ src/main/kotlin/montaine/guillaume/Manga.kt
@@ -2,22 +2,13 @@ package org.example.montaine.guillaume.montaine.guillaume
 
 import kotlin.math.round
 
-data class Manga(val id: String, val titre: String, val prix: Double) {
+data class Manga(val titre: String, val prix: Double) {
     init {
         if (prix <= 0) {
             throw IllegalArgumentException("Le prix doit être positif")
         }
-        if(prix != round(prix*100)/100){
+        if (prix != round(prix * 100) / 100) {
             throw IllegalArgumentException("Le prix ne peux avoir que deux décimales maximum")
-        }
-        mangas[id] = this
-    }
-
-    companion object {
-        private val mangas = mutableMapOf<String, Manga>()
-
-        operator fun invoke(id: String): Manga {
-            return mangas[id] ?: throw IllegalArgumentException("Manga $id inconnu")
         }
     }
 }
