5d9597e961d41dc6f0cec8d2ccec0476f4be382f
tclement0922 <git@tclement.dev>
2024-12-09 15:28:45 +0100

    Main.kt sprint 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,22 @@
 package org.example.montaine.guillaume
 
+import org.example.montaine.guillaume.montaine.guillaume.Manga
+import org.example.montaine.guillaume.montaine.guillaume.Panier
+import org.example.montaine.guillaume.montaine.guillaume.Pays
+
 fun main() {
-    println("Hello World!")
+    val panier1 = Panier(Pays.FRANCE)
+    repeat(19) {
+        panier1.ajouterManga(Manga("1", "Manga1", 3.99))
+    }
+    println("Total panier1: ${panier1.total}")
+
+    val panier2 = Panier(Pays.FRANCE)
+    repeat(42) {
+        panier2.ajouterManga(Manga("2", "Manga2", 3.92))
+    }
+    repeat(82) {
+        panier2.ajouterManga(Manga("3", "Manga3", 4.1))
+    }
+    println("Total panier2: ${panier2.total}")
 }
